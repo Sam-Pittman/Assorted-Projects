07be7768d8d392b574b510f9eaaa9a00e1b3096b
Sam-Pittman <165956740+Sam-Pittman@users.noreply.github.com>
2024-04-10 14:29:29 -0700

    Update a3.py

--- Online Publishing Journal/a3.py
+++ Online Publishing Journal/a3.py
@@ -1,6 +1,6 @@
 # a3.py
 
-# Starter code for assignment 3 in ICS 32 Programming with Software Libraries in Python
+# Starter code for assignment 3 in I/C/S 3/2 Programming with Software Libraries
 
 # Replace the following placeholders with your information.
 
